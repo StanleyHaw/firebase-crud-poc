5331ce880c9a7946ff588fce3a74f2ad9d5dfc8a
StanleyHaw <stanleyhaw94@gmail.com>
2024-05-08 21:10:16 +0800

    feat: Fetched firebase API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import { getData } from './utils/api';
+import { DataListProps } from 'types/index';
 
 function App() {
+  const [dataList, setDataList] = useState<DataListProps[] | undefined>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data: DataListProps[] | undefined = await getData();
+      setDataList(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1 className={'text-red-500 text-4xl'}>Hello World!</h1>
+      {dataList?.map((data) => {
+        const { id, name, age, gender, country } = data;
+        return (
+          <div key={id}>
+            <p>{name}</p>
+            <p>{age}</p>
+            <p>{gender}</p>
+            <p>{country}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
---
+++ src/types/index.d.ts
@@ -0,0 +1,9 @@
+type DataListProps = {
+  id: string;
+  name: string;
+  age: number;
+  gender: string;
+  country: string;
+};
+
+export { DataListProps };
