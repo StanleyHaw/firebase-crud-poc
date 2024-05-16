106e3f17cc004b27c34c6dc1844511cebfb0a8fa
StanleyHaw <stanleyhaw94@gmail.com>
2024-05-16 16:44:08 +0800

    feat: Created DataTable component and separated ProfileForm from FormDialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,61 @@
 import { useState, useEffect } from 'react';
+import { Button } from '@components/ui/Button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@components/ui/Dialog';
+import DataTable from '@components/DataTable';
+import ProfileForm from '@components/ui/ProfileForm';
+import { Data } from '@/types';
 import { getData } from '@utils/api';
-import { Data } from 'types/index';
-import FormDialog from './components/FormDialog';
+import { genderList } from './utils/constant';
+
+type AddDialog = {
+  defaultValues: Data;
+  onSubmitData: (values: Data) => void;
+};
+
+function AddDialog({ defaultValues, onSubmitData }: AddDialog) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleFormSubmit = (isSuccessful: boolean) => {
+    setIsOpen(isSuccessful);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className="w-[calc(180px)] text-white bg-indigo-600 hover:text-white hover:bg-indigo-700"
+        >
+          Add User
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add new user</DialogTitle>
+          <DialogDescription>Please fill in the following information. Once completed, press Submit.</DialogDescription>
+        </DialogHeader>
+        <ProfileForm defaultValues={defaultValues} onSubmitData={onSubmitData} onSubmitSuccess={handleFormSubmit} />
+      </DialogContent>
+    </Dialog>
+  );
+}
 
 function App() {
   const [dataList, setDataList] = useState<Data[] | undefined>([]);
+  const emptyValues = {
+    id: '',
+    name: '',
+    age: 0,
+    gender: genderList[0],
+    country: ''
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,10 +66,23 @@ function App() {
     fetchData();
   }, []);
 
+  const handleAddUser = (data: Data) => {
+    console.log(data, 'added');
+  };
+
+  const handleEditUser = (data: Data) => {
+    console.log(data, 'edited');
+  };
+
+  const handleDelete = (id: string) => {
+    console.log(id, 'deleted');
+  };
+
   return (
-    <>
-      <FormDialog />
-    </>
+    <div className="container mx-auto py-8 space-y-4">
+      <AddDialog defaultValues={emptyValues} onSubmitData={handleAddUser} />
+      <DataTable dataList={dataList} onSubmitEditedData={handleEditUser} onDeleteData={handleDelete} />
+    </div>
   );
 }
 
