b212cbb70229351e765b860b51eeb36e85e54990
StanleyHaw <stanleyhaw94@gmail.com>
2024-05-16 22:05:38 +0800

    feat: Added Firebase data insertion and UI improvements

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,26 @@
 import { collection, getDocs } from 'firebase/firestore';
+import { setDoc, doc } from 'firebase/firestore';
 import { db } from '@config/firebase';
-import { DataListProps } from 'types/index';
+import { Data } from 'types/index';
+import { v4 as uuidv4 } from 'uuid';
 
 export const getData = async () => {
   try {
     const querySnapshot = await getDocs(collection(db, 'UserData'));
-    const data: DataListProps[] = querySnapshot.docs.map((doc) => doc.data() as DataListProps);
+    const data: Data[] = querySnapshot.docs.map((doc) => doc.data() as Data);
     return data;
   } catch (error) {
     console.error('Failed to fetch data: ', error);
+  }
+};
+
+export const addNewData = async (newData: Data) => {
+  try {
+    const newDataId = uuidv4().split('-')[0];
+    const result = { ...newData, id: newDataId };
+    const docRef = doc(db, 'UserData', newDataId);
+    await setDoc(docRef, result);
+  } catch (error) {
+    console.error('Failed to add data: ', error);
   }
 };
