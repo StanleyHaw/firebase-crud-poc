deba217622a13eabf1193c687a346487dbed6e57
StanleyHaw <stanleyhaw94@gmail.com>
2024-05-17 22:30:28 +0800

    Reapply "Merge pull request #3 from StanleyHaw/feature/functional"
    This reverts commit d3894be2f85d9d6116f8857a1d2988c0ca3bd55e.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   css: {
     postcss: {
       plugins: [tailwindcss()]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/')
     }
   }
 });
