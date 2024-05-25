d465227ee3eb9943120884f0f922676df2e26274
StanleyHaw <stanleyhaw94@gmail.com>
2024-05-25 21:39:14 +0800

    feat: Created home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import HomePage from '@/pages/HomePage';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
